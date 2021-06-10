78adc0701a4eeb7aec2a12486050cf9600709c27
eve <eveshino13@gmail.com>
2021-06-11 00:15:04 +0900

    color

--- src/components/cards/links/buttons/button.js
+++ src/components/cards/links/buttons/button.js
@@ -4,7 +4,7 @@ export function Button(props) {
   return (
     <>
       <button className={classes.link}>
-        <img className={classes.logo} src={props.src} alt="logo" />
+        {/* <img className={classes.logo} src={props.src} alt="logo" /> */}
         <a href={props.href} target="_blank" rel="noreferrer">
           {props.title}
         </a>
