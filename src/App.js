dd5432e066cc71a9b8c8af94874c275cf3fae278
eve <eveshino13@gmail.com>
2021-06-12 14:54:08 +0900

    redirects

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import React from "react";
 import Home from "./components/pages/Home";
-import Signin from "./components/forms/Signin";
-import Register from "./components/forms/Register";
+
 import Profile from "./components/pages/Profile";
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home}></Route>
-        <Route path="/Signin" component={Signin}></Route>
-        <Route path="/Register" component={Register}></Route>
         <Route path="/aiyu" component={Profile}></Route>
       </Switch>
     </Router>
