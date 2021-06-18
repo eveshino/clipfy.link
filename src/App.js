42546bbe907e9e2269a5a29bcde2062eeb3ec661
eve <eveshino13@gmail.com>
2021-06-18 14:47:51 +0900

    changed the home page for a possible client

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import React from "react";
-import Home from "./components/pages/Home";
-
 import Profile from "./components/pages/Profile";
+import React from "react";
+
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/aiyu" component={Profile}></Route>
+        <Route path="/" component={Profile}></Route>
       </Switch>
     </Router>
   );
