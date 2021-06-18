1532207582dd7ff19db77d3c3868ebda87ba0040
eve <eveshino13@gmail.com>
2021-06-18 15:48:28 +0900

    drill

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Home from "./components/pages/Home";
 import Profile from "./components/pages/Profile";
 import React from "react";
 
@@ -7,6 +7,7 @@ function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/" exact component={Home}></Route>
         <Route path="/aiyu" component={Profile}></Route>
       </Switch>
     </Router>
